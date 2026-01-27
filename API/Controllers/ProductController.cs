54e4a6b482cf27dc65e365176fe130e7172d7b3d
ankjin <mydevact@outlook.com>
2026-01-27 09:22:04 -0600

    End of section 5

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,108 @@
+using API.RequestHelpers;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specification;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ProductsController(IGenericRepository<Product> repo): BaseApiController
+{
+    // private readonly StoreContext context;
+    // public ProductsController(StoreContext context)
+    // {
+    //     this.context = context;
+    // }
+
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery]ProductSpecParam specParams)
+    {
+        var spec = new ProductSpecification(specParams);
+
+        return await CreatePageResult(repo, spec, specParams.PageIndex, specParams.PageSize);
+    }
+        
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await repo.GetByIdAsync(id);
+
+        if(product is null)
+        {
+            return NotFound();
+        }
+
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        repo.Add(product);
+
+        if(await repo.SaveAllAsync())
+        {
+            return CreatedAtAction("GetProduct", new {id = product.Id}, product);
+        }
+
+        return BadRequest("Problem creating product.");
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProduct(int id, Product product)
+    {
+        if(product.Id != id || !ProductExists(id))
+        {
+            return BadRequest("Cannot update this product.");
+        }
+
+        repo.Update(product);
+
+        if(await repo.SaveAllAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Problem updating product.");
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await repo.GetByIdAsync(id);
+
+        if(product == null)
+        {
+            return NotFound();
+        }
+
+        repo.Remove(product);
+        if(await repo.SaveAllAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Problem deleting product.");
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        var spec = new BrandListSpecification();
+
+        return Ok(await repo.ListAsync(spec));
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        var spec = new TypeListSpecification();
+
+        return Ok(await repo.ListAsync(spec));
+    }
+
+    private bool ProductExists(int id)
+    {
+        return repo.Exists(id);
+    }
+
+}
---
+++ Core/Specification/BaseSpecification.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Linq.Expressions;
+using Core.Interfaces;
+
+namespace Core.Specification;
+
+public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
+{
+    // private readonly Expression<Func<T, bool>> criteria;
+    // public BaseSpecification(Expression<Func<T, bool>> criteria)
+    // {
+    //     this.criteria = criteria;
+    // }
+
+    //// This is for (optional) empty constructor
+    protected BaseSpecification(): this(null) {}
+
+
+    public Expression<Func<T, bool>>? Criteria => criteria;
+
+    public Expression<Func<T, object>>? OrderBy { get; private set;}
+
+    public Expression<Func<T, object>>? OrderByDescending { get; private set;}
+
+    public bool IsDistinct { get; private set; }
+
+    public int Take { get; private set; }
+
+    public int Skip { get; private set; }
+
+    public bool IsPagingEnabled { get; private set; }
+
+    public IQueryable<T> ApplyCriteria(IQueryable<T> query)
+    {
+        if(Criteria is not null)
+        {
+            query = query.Where(Criteria);
+        }
+
+        return query;
+    }
+
+    protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+    {
+        OrderBy = orderByExpression;
+    }
+    protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+    {
+        OrderByDescending = orderByDescExpression;
+    }
+    protected void ApplyDistinct()
+    {
+        IsDistinct = true;
+    }
+    protected void ApplyPaging(int skip, int take)
+    {
+        Skip = skip;
+        Take = take;
+        IsPagingEnabled = true;
+    }
+}
+
+public class BaseSpecification<T, TResult>(Expression<Func<T, bool>>? criteria)
+    : BaseSpecification<T>(criteria), ISpecification<T, TResult>
+{
+    protected BaseSpecification(): this(null) {}
+    public Expression<Func<T, TResult>>? Select { get; private set; }
+
+    protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
+    {
+        Select = selectExpression;
+    }
+}
