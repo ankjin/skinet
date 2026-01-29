d3c7ec64787f25f40bf549927986c7ae7c98bda2
ankjin <mydevact@outlook.com>
2026-01-29 10:54:02 -0600

    End of section 8

---
+++ client/src/app/layout/header/header.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatIcon, MatButton, MatBadge  ],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+
+}
