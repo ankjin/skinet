69a5505a08b4a032b92d1e22b0349236fc1242fd
ankjin <mydevact@outlook.com>
2026-02-10 10:21:04 -0600

    End of section 11

--- client/src/app/layout/header/header.ts
+++ client/src/app/layout/header/header.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIcon, MatButton, MatBadge, RouterLink, RouterLinkActive],
+  imports: [MatIcon, MatButton, MatBadge, RouterLink, RouterLinkActive, MatProgressBar],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  busyService = inject(BusyService)
 
 }
