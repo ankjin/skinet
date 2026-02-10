b92cce020304d94832fc20127d72575700c02331
ankjin <mydevact@outlook.com>
2026-02-10 01:35:04 -0600

    End of section 10

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -4,10 +4,12 @@ import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card'
 import { MatIcon } from '@angular/material/icon';
 import { CurrencyPipe } from '@angular/common';
 import { CartService } from '../../../core/services/cart.service';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
-  imports: [MatCard, MatCardContent, MatCardActions, MatIcon, CurrencyPipe],
+  imports: [MatCard, MatCardContent, MatCardActions, MatIcon, CurrencyPipe, MatButton, RouterLink],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css',
 })
