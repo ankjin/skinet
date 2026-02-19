ae666ea7a327e6d951abfe4fd01f54da44ab74c2
ankjin <mydevact@outlook.com>
2026-02-19 00:46:40 -0600

    End of section 15

---
+++ client/src/app/shared/components/empty-state/empty-state.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { BusyService } from '../../../core/services/busy.service';
+
+@Component({
+  selector: 'app-empty-state',
+  imports: [MatIcon, MatButton, RouterLink],
+  templateUrl: './empty-state.component.html',
+  styleUrl: './empty-state.component.css',
+})
+export class EmptyStateComponent {
+  busyService = inject(BusyService);
+  // message = input.required<string>();
+  // icon = input.required<string>();
+  // actionText = input.required<string>();
+  // action = output<void>();
+
+  onAction() {
+    //this.action.emit();
+  }
+}
